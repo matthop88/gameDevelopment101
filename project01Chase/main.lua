a01aa23a061815b60d485a2a4031d2b7d6eb8e9d
Matthew Hopkins <matthop88@gmail.com>
2025-05-21 16:53:25 -0700

    Changed order of draw eye functions so that outlines don't link

--- project01Chase/main.lua
+++ project01Chase/main.lua
@@ -119,9 +119,11 @@ function drawMonster()
     love.graphics.circle("line", monsterX, monsterY, 75, 75)
     love.graphics.setColor(COLOR_WHITE)
     love.graphics.circle("fill", monsterEye1X, monsterEye1Y, 20, 20)
-    love.graphics.circle("fill", monsterEye2X, monsterEye2Y, 20, 20)
     love.graphics.setColor(COLOR_BLACK)
     love.graphics.circle("line", monsterEye1X, monsterEye1Y, 20, 20)
+    love.graphics.setColor(COLOR_WHITE)
+    love.graphics.circle("fill", monsterEye2X, monsterEye2Y, 20, 20)
+    love.graphics.setColor(COLOR_BLACK)
     love.graphics.circle("line", monsterEye2X, monsterEye2Y, 20, 20)
     
 end
